4a7575b0f2c688ab58ec65e6d2f4644eb33e63e9
Silen Resras <mugafelhida98@gmail.com>
2024-10-03 01:28:56 +0300

    third commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 7000
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: 'https://mern-full-authentication-frontend.vercel.app', credentials: true }))
 
 //middleware
 app.use(express.json())
