ca545766677cbcf1cba5b4a0d13ffd3bdcbf4cec
Silen Resras <mugafelhida98@gmail.com>
2024-10-03 01:08:50 +0300

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import path from 'path';
 
 import { connectDB } from './db/connectDB.js';
 import router from './routes/auth.route.js';
@@ -12,7 +11,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 7000
 const app = express()
-const __dirname = path.resolve()
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
@@ -23,15 +21,6 @@ app.use(cookieParser()) //allows us to parse incoming cookies
 
 //routing  
 app.use("/api/auth", authRoutes)
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-}
 
 
 
