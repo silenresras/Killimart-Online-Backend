e6aebd3a24fe5c67565879dee6b43129f1d097ef
Silen Resras <mugafelhida98@gmail.com>
2024-10-05 00:03:14 +0300

    fixed typerscript declarations for modules

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 7000
 const app = express()
 
 app.use(cors({
-    origin: "https://localhost:5174",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
