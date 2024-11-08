17118131fac516106a8376faa771d94cbdfad96e
Silen Resras <mugafelhida98@gmail.com>
2024-11-08 23:59:27 +0300

    fixed the token configurations for auths

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -5,7 +5,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',  //if in production mode, use secure cookie, otherwise not
-        sameSite: 'strict',
+        sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
