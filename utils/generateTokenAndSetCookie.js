106c152cfe1df48776223bddaecd44678b6b338b
RESRAS SILEN <resrassilen@RESRASs-MacBook-Pro.local>
2025-07-21 23:48:20 -0700

    remove domain in cookie settings

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -7,10 +7,10 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",   // true on Render
+        secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        domain: process.env.NODE_ENV === "production" ? ".onrender.com" : undefined,
+        // domain: undefined   // <- remove this line
       });
 
     return token;
